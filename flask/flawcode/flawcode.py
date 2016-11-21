e8bbd32b9ac5f50f472b58f0cf3b09e93d6d097b
joydeep bhattacharjee <joydeepubuntu@gmail.com>
2016-11-21 21:55:53 +0530

    naming

--- flask/flawcode/flawcode.py
+++ flask/flawcode/flawcode.py
@@ -17,12 +17,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    #return redirect(url_for('episode/show/4'))
-    return redirect(url_for('hello', ep_no='4'))
+    return redirect(url_for('shows', ep_no='4'))
 
 
 @app.route('/episode/show/<ep_no>')
-def hello(ep_no):
+def shows(ep_no):
     return render_template("hello.html", name=ep_no)
 
 
