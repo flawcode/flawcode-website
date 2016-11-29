34b0490b10cb43064d73a7af8918ac9fc113dd49
infinite-Joy <joydeepubuntu@gmail.com>
2016-11-29 20:19:46 +0530

    rename flawcode to views

--- flask/flawcode/views.py
+++ flask/flawcode/views.py
@@ -12,8 +12,6 @@ from flask import render_template
 from flask import redirect
 from flask import url_for
 
-app = Flask(__name__)
-
 
 @app.route('/')
 def index():
@@ -28,7 +26,3 @@ def shows(ep_no):
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
-
-
-if __name__ == "__main__":
-    app.run()
