383ccdd528d9a49abb04da2f4018b9c9ee376012
Joydeep Bhattacharjee <joydeepubuntu@gmail.com>
2017-04-29 09:28:44 +0530

    fixing the test paths

--- tests/test_helper.py
+++ tests/test_helper.py
@@ -1,12 +1,12 @@
 from core.helpers import show_notes
-from core.helpers import directly_linked_old
+from core.helpers import get_last_4episode_num
 
 
 def test_show_notes():
     res = show_notes(4)
-    assert len(list(res)) == 3
+    assert len(list(res)) == 5446
 
 
 def test_directly_linked_old():
-    res = list(directly_linked_old(10))
+    res = list(get_last_4episode_num(10))
     assert res == [10, 9, 8, 7]
