16127f15a7447df02d3b4c7d066d27e2202c524b
mihirsd <35537937+mihirsd@users.noreply.github.com>
2018-01-27 15:36:05 +0530

    Add manage.py

---
+++ manage.py
@@ -0,0 +1,20 @@
+from flask_script import Manager
+from flask_migrate import Migrate, MigrateCommand
+
+from core.api import app, db
+
+migrate = Migrate(app, db)
+manager = Manager(app)
+
+manager.add_command('db', MigrateCommand)
+
+@manager.command
+def create_db():
+    db.create_all()
+
+@manager.command
+def drop_db():
+    db.drop_all()
+
+if __name__ == '__main__':
+    manager.run()
