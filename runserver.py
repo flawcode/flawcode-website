54f7269b5712d345c6eaed0e619c30926be5ddf8
Joydeep Bhattacharjee <joydeepubuntu@gmail.com>
2018-02-18 17:59:38 +0530

    some documentation changes to runserver

--- runserver.py
+++ runserver.py
@@ -1,3 +1,10 @@
+"""
+The driver of the flawcode application.
+Make changes here in case you want to change the settings.
+"""
+
+__author__ = 'joydeep bhattacharjee'
+
 from core.api import app
 
 if __name__ == '__main__':
