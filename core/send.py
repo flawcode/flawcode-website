e3627cf37895a7304f2660eeb7d3315acf306614
mihirsd <35537937+mihirsd@users.noreply.github.com>
2018-02-26 01:27:19 +0530

    Clean up send.py

--- core/send.py
+++ core/send.py
@@ -14,38 +14,46 @@ from settings import MAIL_PASSWORD
 from settings import SECRET_KEY
 from settings import UNSUBSCRIBE_TOKEN_SALT
 
+serializer = URLSafeTimedSerializer(SECRET_KEY)
+
 conn = sqlite3.connect(DB_FILENAME+'.db')
 
 cursor = conn.execute('SELECT email, confirmed FROM user')
 
-serializer = URLSafeTimedSerializer(SECRET_KEY)
-
-epno = str(EPISODE_COUNT)
-base_url = BASE_URL
-
 for row in cursor:
+	
 	user_email = row[0]
 	user_confirmed = bool(row[1])
+	
 	token = str(serializer.dumps(user_email, salt=UNSUBSCRIBE_TOKEN_SALT))
-	ep_url = base_url + '/episode/show/'+ epno
-	remove_url = base_url + '/unsubscribe/' + token
+	
+	ep_url = BASE_URL + '/episode/show/'+ str(EPISODE_COUNT)
+	remove_url = BASE_URL + '/unsubscribe/' + token
+	
 	print('Sending email to', user_email)
+	
 	if user_confirmed:
+		
 		message_content = """\
 		Hello!
 		FlawCode has published a new podcast! <a href=\"http://{ep_url}\">Check it out!</a>
 		<br><br><hr><br>
 		Do not wish to receive updates from FlawCode Podcasts? Please <a href=\"http://{remove_url}\">unsubscribe.</a>
 		""".format(ep_url=ep_url, remove_url=remove_url)
+		
 		message = MIMEText(message_content, 'html')
-		message['From'] = MAIL_USERNAME
-		message['To'] = user_email
+		
+		message['From'] = MAIL_USERNAME		
+		message['To'] = user_email		
 		message['Subject'] = 'New episode published at FlawCode Podcasts!'
+		
 		s = smtplib.SMTP(MAIL_SERVER, MAIL_PORT)		
 		s.starttls()
 		s.login(MAIL_USERNAME, MAIL_PASSWORD)
 		s.sendmail(MAIL_USERNAME, user_email, message.as_string())
 		s.quit()
+		
 		print('Successful.')
+		
 	else:
 		print('Failure. User has not verified email address.')
