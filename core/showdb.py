adfe1c151d82e421747aac196dc1e11e2f3e2917
mihirsd <35537937+mihirsd@users.noreply.github.com>
2018-02-28 20:35:16 +0530

    Add showdb.py
    Added a script to view database entries

---
+++ core/showdb.py
@@ -0,0 +1,17 @@
+
+import sqlite3
+
+from settings import DB_FILENAME
+
+conn = sqlite3.connect(DB_FILENAME+'.db')
+
+cursor = conn.execute('SELECT * FROM user')
+
+for row in cursor:
+	print('------------------------------------------------------')
+	print('id:\t\t\t', row[0])
+	print('email:\t\t\t', row[1])
+	print('confirmation_sent_on:\t', row[2])
+	print('confirmed:\t\t', row[3])
+	print('confirmed_on:\t\t', row[4])
+	print('------------------------------------------------------')
