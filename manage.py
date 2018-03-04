28c501d2e64ad2da35c10ca36a360ed9b58735dd
Joydeep Bhattacharjee <joydeepubuntu@gmail.com>
2018-03-04 16:26:46 +0530

    formatting

--- manage.py
+++ manage.py
@@ -9,13 +9,16 @@ manager = Manager(app)
 
 manager.add_command('db', MigrateCommand)
 
+
 @manager.command
 def create_db():
     db.create_all()
 
+
 @manager.command
 def drop_db():
     db.drop_all()
+
 
 if __name__ == '__main__':
     manager.run()
